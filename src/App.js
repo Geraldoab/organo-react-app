9b45f374fa6967804b665c20108dffd4576466ba
Geraldo Aparecido Borges <geraldo.estudos.trabalho@gmail.com>
2023-11-10 19:57:11 -0300

    added a new form to create teams

--- src/App.js
+++ src/App.js
@@ -2,41 +2,51 @@ import Banner from './components/Banner';
 import Form from './components/Form';
 import { useState } from 'react'
 import Team from './components/Team';
+import { v4 as uuidv4 } from 'uuid';
+import Snackbar from '@mui/material/Snackbar'; 
+import Alert from '@mui/material/Alert'; 
 
 function App() {
 
   const teams = [
         {
+          id: uuidv4(),
           name: 'Computer programming',
           primaryColor: '#57C278',
           secondaryColor: '#D9F7E9',
         },
         {
+          id: uuidv4(),
           name: 'Front-End',
           primaryColor: '#82CFFA',
           secondaryColor: '#E8F8FF',
         },
         {
+          id: uuidv4(),
           name: 'Data Science',
           primaryColor: '#A6D157',
           secondaryColor: '#F0F8E2',
         },
         {
+          id: uuidv4(),
           name: 'Devops',
           primaryColor: '#E06B69',
           secondaryColor: '#FDE7E8',
         },
         {
+          id: uuidv4(),
           name: 'UX e Design',
           primaryColor: '#D86EBF',
           secondaryColor: '#FAE5F5',
         },
         {
+          id: uuidv4(),
           name: 'Mobile',
           primaryColor: '#FEBA05',
           secondaryColor: '#FFF5D9',
         },
         {
+          id: uuidv4(),
           name: 'Management',
           primaryColor: '#FF8A29',
           secondaryColor: '#FFEEDF',
@@ -44,6 +54,7 @@ function App() {
   ]
 
   const [employeeList, setEmployee] = useState([])
+  const [teamList, setTeam] = useState(teams)
 
   const onEmployeeCreated = (employee) => {
     setEmployee([...employeeList, employee])
@@ -51,17 +62,58 @@ function App() {
     console.log(employeeList)
   }
 
+  const onTeamCreated = (team) => {
+    const found = teamList.filter(t=> t.name.toLowerCase()===team.name.toLowerCase())
+    if(found.length === 0) {
+      setTeam([...teamList, team])
+      console.log(teamList)
+      onOpenSnackbarClickHandler('Saved!', 'success')
+    }
+    else {
+      onOpenSnackbarClickHandler('Cannot save the Team because it already exists!', 'info')
+    }
+  }
+
   const deleteEmployee = (id) => {
     setEmployee(employeeList.filter(employee => employee.id !== id))
   }
 
+  const [showSnackbar, setShowSnackbar] = useState(false); 
+  const [snackBarMessage, setSnackBarMessage] = useState(false); 
+  const [snackBarType, setSnackBarType] = useState(false); 
+  
+  const onOpenSnackbarClickHandler = (message, type) => { 
+      setShowSnackbar(true); 
+      setSnackBarMessage(message)
+      setSnackBarType(type)
+  }; 
+
+  const onCloseSnackbarClickHandler = (event) => { 
+      setShowSnackbar(false); 
+  }; 
+
   return (
     <div className="App">
+      <Snackbar 
+          open={showSnackbar} 
+          onClose={onCloseSnackbarClickHandler}  
+          autoHideDuration={3000}
+          anchorOrigin={{ horizontal: 'center', vertical: 'top' }}>
+          <Alert severity={snackBarType} sx={{ width: '100%' }}>
+              {snackBarMessage}
+          </Alert>
+      </Snackbar>
       <Banner />
-      <Form teams={teams.map(team => team.name)} onCreated={employee => {
-        onEmployeeCreated(employee)
-      }} />
-      {teams.map(team => 
+      <Form 
+        teams={teamList.map(team => team.name)} 
+        onCreated={employee => {
+          onEmployeeCreated(employee)
+        }}
+        onTeamCreated={team => {
+          onTeamCreated(team)
+        }} 
+      />
+      {teamList.map(team => 
         <Team 
           key={team.name} 
           name={team.name} 
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -4,6 +4,9 @@ import TextField from '../TextField'
 import './Form.css'
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import { MaterialPicker  } from 'react-color'
+import Snackbar from '@mui/material/Snackbar'; 
+import Alert from '@mui/material/Alert'; 
 
 const Form = (props) => {
 
@@ -11,6 +14,9 @@ const Form = (props) => {
     const [role, setRole] = useState('')
     const [photo, setPhoto] = useState('')
     const [team, setTeam] = useState('')
+    const [teamName, setTeamName] = useState('')
+    const [teamPrimaryColor, setTeamPrimaryColor] = useState('#F00F00')
+    const [teamSecondaryColor, setTeamSecondaryColor] = useState('#F00F00')
 
     const onSave = (event) => {
         event.preventDefault()
@@ -26,10 +32,46 @@ const Form = (props) => {
         setRole('')
         setPhoto('')
         setTeam('')
+
+        onOpenClickHandler()
     }
+
+    const onSaveTeam = (event) => {
+        event.preventDefault()
+        props.onTeamCreated({
+            id: uuidv4(),
+            name: teamName,
+            primaryColor: teamPrimaryColor,
+            secondaryColor: teamSecondaryColor
+        })
+
+        setTeamPrimaryColor('#F00F00')
+        setTeamSecondaryColor('#F00F00')
+        setTeamName('')
+    }
+
+    const [showSnackbar, setShowSnackbar] = useState(false); 
+  
+    const onOpenClickHandler = () => { 
+        setShowSnackbar(true); 
+    }; 
+  
+    const onCloseClickHandler = (event) => { 
+        setShowSnackbar(false); 
+    }; 
     
     return (
         <section className="form">
+            <Snackbar 
+                open={showSnackbar} 
+                onClose={onCloseClickHandler}  
+                autoHideDuration={3000}
+                anchorOrigin={{ horizontal: 'center', vertical: 'top' }}>
+                <Alert severity='success' sx={{ width: '100%' }}>
+                    Saved!
+                </Alert>
+            </Snackbar>
+
             <form onSubmit={onSave}>
                 <h2>Please fill the form to add a new employee.</h2>
                 <TextField
@@ -62,6 +104,26 @@ const Form = (props) => {
                     required={true} 
                     label="Team" 
                     items={props.teams} />
+
+                <ButtonField>Add</ButtonField>
+            </form>
+            <form onSubmit={onSaveTeam}>
+                <h2>Please fill the form to add a new Team.</h2>
+                <TextField
+                     autoComplete='off'
+                     value={teamName}
+                     onType={value => setTeamName(value)}   
+                     required={true} 
+                     label="Team name" 
+                     placeholder="Type the team name" />
+
+                <label className='team-color'>Primary color</label>
+                <MaterialPicker color={teamPrimaryColor} onChange={value => setTeamPrimaryColor(value.hex)} />
+                <br />  
+
+                <label className='team-color'>Secondary color</label>
+                <MaterialPicker color={teamSecondaryColor} onChange={value => setTeamSecondaryColor(value.hex)} />
+                <br />   
 
                 <ButtonField>Add</ButtonField>
             </form>
